915e1f6e237bedb801e078d0f13c4d6ada6d43d4
Chris Grant <chrisgrant303@gmail.com>
2018-09-16 14:29:47 -0500

    still having CSS issues

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -14,21 +16,26 @@ var PORT = process.env.PORT || 8080;
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(methodOverride('_method'));
+
+
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
 // Starts the server to begin listening
 // =============================================================
