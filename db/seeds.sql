6a97a37de7512727a7f9b27a1796bf43005fa2cd
Chris Grant <chrisgrant303@gmail.com>
2018-09-15 21:45:24 -0500

    database uploaded

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values ("Triple Heart Attack", 0);
+insert into burgers (burger_name, devoured) values ("Revenge of the Jalapeno", 0);
+insert into burgers (burger_name, devoured) values ("Mustart and Cheese Only Cheeseburger", 0);
